6bbee8c4237079a2ffd1b404428429d8bce9972f
Hiroshi-iida <64621519+Hiroshi-iida@users.noreply.github.com>
2020-05-01 13:12:30 +0900

    Add files via upload

---
+++ kuku.java
@@ -0,0 +1,18 @@
+package javaStudy1;
+
+public class kuku {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		for(int a = 1; a<=9; a++ ) {
+			for(int i =1; i<=9 ; i++){
+				System.out.print(a * i+" ");
+
+		}
+			System.out.print("\n");
+		}
+	}
+
+}
